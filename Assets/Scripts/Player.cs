00da9f4a4438d6f3a15db04d65a92088998d50fc
oshoham <oshoham@oberlin.edu>
2012-10-16 14:38:31 -0400

    Actually added some more scripts.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	void Start() {
+
+	}
+
+	void Update() {
+
+	}
+}
---
+++ Assets/Scripts/Robot.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using System.Collections;
+
+public class Robot : MonoBehaviour {
+
+	void Start() {
+
+	}
+
+	void Update() {
+
+	}
+}
