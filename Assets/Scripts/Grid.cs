a93cc0c5b4bf26d8255fae440928b8848c845ae1
oshoham <oshoham@oberlin.edu>
2012-10-16 10:55:51 -0400

    Started adding some methods to Grid.cs.

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -1,7 +1,9 @@
 using UnityEngine;
 using System.Collections;
 
-public class GameManager : MonoBehaviour {
+public class Grid : MonoBehaviour {
+
+	Square[][] grid;
 
 	void Start() {
 
@@ -9,5 +11,36 @@ public class GameManager : MonoBehaviour {
 
 	void Update() {
 
+	}
+
+	// Check to see if the Sqaure at the given coordinates is occupied by anything
+	bool Check(Vector2 coord) {
+		if(grid[coord.x][coord.y].objects.Count > 0)
+			return true;
+		return false;
+	}
+	
+	// Check to see if the Square at the given coordinates is occupied by a Wall
+	bool CheckWall(Vector2 coord) {
+		Square sq = grid[coord.x][coord.y];
+		if(sq.objects.Count > 0) {
+			foreach(GameObject g in sq.objects) {
+				if(g.GetComponent<Wall>() != null)
+					return true;
+			}
+		}
+		return false;
+	}
+
+	// Get a GameObject of a particular type at a particular (x,y) if there is one
+	GameObject Get(Vector2 coord, String type) {
+		Square sq = grid[coord.x][coord.y];
+		if(sq.objects.Count > 0) {
+			foreach(GameObject g in sq.objects) {
+				if(g.GetComponent(type) != null)
+					return g;
+			}
+		}
+		return null;
 	}
 }
--- Assets/Scripts/Square.cs
+++ Assets/Scripts/Square.cs
@@ -1,7 +1,13 @@
 using UnityEngine;
 using System.Collections;
 
-public class GameManager : MonoBehaviour {
+public class Square : MonoBehaviour {
+
+	Vector2 coordinates;	// (x,y) coordinates within the Grid
+	List<GameObject> objects;	// list of GameObjects occupying this Square
+	Square[] neighbors;	// array of adjacent Squares within the Grid
+	Vector3 worldCoordinates;	// 3D coordinates in Unity's world units
+	Plane plane;	// the actual plane that this Square represents
 
 	void Start() {
 
