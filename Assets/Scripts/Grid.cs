2fd6b24846c4e4e96d8adc35864cf7f0569abe09
oshoham <oshoham@oberlin.edu>
2012-10-16 13:31:52 -0400

    Apparently 2D arrays in C# are declared using [,] not [][]

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class Grid : MonoBehaviour {
 
-	Square[][] grid; // 2D array of Squares that underlies the Grid class
+	Square[,] grid; // 2D array of Squares that underlies the Grid class
 	int width; // self-explanatory
 	int height; // self-explanatory
 
@@ -11,7 +11,8 @@ public class Grid : MonoBehaviour {
 	public Grid(int width, int height) {
 		this.width = width;
 		this.height = height;
-		grid = new Square[width][height];
+		grid = new Square[width, height];
+
 		for(int i = 0; i < width; i++) {
 			for(int j = 0; j < height; j++) {
 				Vector2 coordinates = new Vector2(i, j);
@@ -30,14 +31,14 @@ public class Grid : MonoBehaviour {
 
 	// Check to see if the Sqaure at the given coordinates is occupied by anything
 	bool Check(Vector2 coord) {
-		if(grid[coord.x][coord.y].objects.Count > 0)
+		if(grid[coord.x, coord.y].objects.Count > 0)
 			return true;
 		return false;
 	}
 	
 	// Check to see if the Square at the given coordinates is occupied by a Wall
 	bool CheckWall(Vector2 coord) {
-		Square sq = grid[coord.x][coord.y];
+		Square sq = grid[coord.x, coord.y];
 		if(sq.objects.Count > 0) {
 			foreach(GameObject g in sq.objects) {
 				if(g.GetComponent<Wall>() != null)
@@ -48,8 +49,8 @@ public class Grid : MonoBehaviour {
 	}
 
 	// Get a GameObject of a particular type at a particular (x,y) if there is one
-	GameObject Get(Vector2 coord, String type) {
-		Square sq = grid[coord.x][coord.y];
+	GameObject Get(Vector2 coord, string type) {
+		Square sq = grid[coord.x, coord.y];
 		if(sq.objects.Count > 0) {
 			foreach(GameObject g in sq.objects) {
 				if(g.GetComponent(type) != null)
@@ -61,8 +62,8 @@ public class Grid : MonoBehaviour {
 
 	// Move a given GameObject from one Square to another (does not handle animation)
 	void Move(Vector2 start, Vector2 end, GameObject mover) {
-		Square sq1 = grid[start.x][start.y];
-		Square sq2 = grid[end.x][end.y];
+		Square sq1 = grid[start.x, start.y];
+		Square sq2 = grid[end.x, end.y];
 		if(sq1.objects.Count == 0)
 			return;
 		foreach(GameObject g in sq1.objects) {
