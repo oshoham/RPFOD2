f88fb245614998bdce058207bbdac724b7b28af4
vorDd <k.e.aungst@gmail.com>
2012-10-28 16:32:09 -0400

    careful

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
 using System.Collections;
 
-public class Projectile : MonoBehaviour {
+public class Projectile : MonoBehaviour, IColor {
 
 	public Vector2 gridCoords;
 	public Vector2 dir;
+	public Color colorPainted{ get; set; }
+	public GameObject cameFrom;
+
 
 	void Start() {
 
@@ -21,12 +24,22 @@ public class Projectile : MonoBehaviour {
 			gridCoords += coords;
 	}
 
-	public static GameObject MakeProj(int x, int y, Vector3 pos, Vector2 dir) {
+	public static GameObject MakeProj(int x, int y, Vector3 pos, Vector2 dir, Color col, GameObject cameFrom) {
 		GameObject proj = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+		proj.transform.localScale = new Vector3(.0625f, .0625f, .0625f);
+		proj.name = "Projectile";
 		proj.transform.position = pos;
 		Projectile script = proj.AddComponent<Projectile>();
 		script.gridCoords = new Vector2(x, y);
 		script.dir = dir;
+		script.colorPainted = col;
+		proj.AddComponent<Rigidbody>();
+		proj.rigidbody.isKinematic = false;
+		proj.rigidbody.useGravity = false;
+		proj.rigidbody.constraints = RigidbodyConstraints.FreezeAll;
+		proj.rigidbody.collider.isTrigger = true;
+		proj.rigidbody.collider.enabled = true;
+		script.cameFrom = cameFrom;
 		return proj;
 	}
 }
