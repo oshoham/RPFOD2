36e601b3a7e20dcad166fef81b400571ea03bb16
vorDd <k.e.aungst@gmail.com>
2012-10-29 18:47:53 -0400

    Projectile updates

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
+using System;
 
 public class Projectile : MonoBehaviour, IColor {
 
@@ -7,13 +8,28 @@ public class Projectile : MonoBehaviour, IColor {
 	public Vector2 dir;
 	public Color colorPainted{ get; set; }
 	public GameObject cameFrom;
-
-
+	public static List<string> attack = new List<string>(new string[] {"Robot", "Player"});
+	public int life = (GameManager.WIDTH > GameManager.HEIGHT) ? GameManager.WIDTH : GameManager.HEIGHT;
+	public Grid grid;
 	void Start() {
-
 	}
 	
 	void Update() {
+		List<GameObject> canvas = grid.GetObjectsOfTypes(gridCoords, attack);
+	
+		if(canvas != null)
+		{
+			foreach(GameObject obj in canvas) {
+				if(obj.name == "Robot")
+					obj.GetComponent<Robot>().colorPainted = colorPainted;
+				else if(obj.name == "Player")
+					obj.GetComponent<Player>().colorPainted = colorPainted;
+			}
+			Destroy(this);
+		}
+		life--;
+		if(life == 0)
+			Destroy(this);
 	}
 	
 
@@ -25,7 +41,7 @@ public class Projectile : MonoBehaviour, IColor {
 		}
 	}
 
-	public static GameObject MakeProj(int x, int y, Vector3 pos, Vector2 dir, Color col, GameObject cameFrom) {
+	public static GameObject MakeProj(int x, int y, Vector3 pos, Vector2 dir, Color col, GameObject cameFrom, Grid grid) {
 		GameObject proj = GameObject.CreatePrimitive(PrimitiveType.Sphere);
 		proj.transform.localScale = new Vector3(.0625f, .0625f, .0625f);
 		proj.name = "Projectile";
@@ -34,7 +50,8 @@ public class Projectile : MonoBehaviour, IColor {
 		script.gridCoords = new Vector2(x, y);
 		script.dir = dir;
 		script.colorPainted = col;
-		script.cameFrom = cameFrom;
+		script.cameFrom = cameFrom; 
+		script.grid = grid;
 		return proj;
 	}
 
