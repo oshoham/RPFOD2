bd65dffe1770f5ce455f05a5ed3d051c93439134
vorDd <k.e.aungst@gmail.com>
2012-11-02 10:30:55 -0400

    level add

---
+++ Assets/Resources/Scripts/Level.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Level {
+
+	public Level(){}
+
+	public void L1()
+	{
+		L1 lev = new L1();
+	}
+
+	public void MakeWalls() {}
+	public void MakeBots() {}
+	public void MakeBelts() {}
+	public void MakeTraps() {}
+	public void MakePaint() {}
+
+}
