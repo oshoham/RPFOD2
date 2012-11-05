6bdf2c0289024294183515a5af380890b05404fa
oshoham <oshoham@oberlin.edu>
2012-11-05 13:47:44 -0500

    Continuing to work on LevelWriter.cs.

--- Assets/Resources/Scripts/LevelWriter.cs
+++ Assets/Resources/Scripts/LevelWriter.cs
@@ -7,18 +7,43 @@ using System.Windows.Forms;
 
 public static class LevelWriter {
 	
-	public static void WriteLevel() {
-
-	}
-
-	public static void CreateFile(string filename) {
+	public static void WriteLevel(string filename) {
 		string path = "";
 		FolderBrowserDialog browser = new FolderBrowserDialog();
 		if(broswer.ShowDialog() == DialogResult.OK)
 			path = browser.SelectedPath;
 		path = Path.Combine(path, filename);
 
+		if(!File.Exists(path)) { //take this line out if we want to overwrite files
+			using (StreamWriter writer = File.CreateText(path)) {
+				for(int i = 0; i < GameManager.floor.width; i++) {
+					for(int j = 0; j < GameManager.floor.height; j++) {
+						StringBuilder sb = new StringBuilder();
+						Square sq = GameManager.floor.grid[i, j];
+						Wall wall = sq.objects.Find(GameObject g => g.GetComponent<Wall>() != null)
+						if(wall != null) {
+							sb.Append(0 + " " + wall.health + " ");
+							if(wall.destructible == false)
+								sb.Append(0 + " ");
+							else
+								sb.Append(1 + " ");
 
+							if(wall.colorPainted == Color.red)
+								sb.Append(0 + " ");
+							else if(wall.colorPainted == Color.green)
+								sb.Append(1 + " ");
+							else if(wall.colorPainted == Color.blue)
+								sb.Append(2 + " ");
+							else
+								sb.Append(3 + " ");
+						}
+
+					}
+				}
+			}
+		}
+		else
+			Debug.Log("Dude, what are you trying to do here? Filename: " + filename + " already exists. Just stop already.");
 	}
 
 }
