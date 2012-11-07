b64291b1cd2c564977daad4bac0285cdf900e9a8
oshoham <oshoham@oberlin.edu>
2012-11-07 17:48:03 -0500

    Added ObjectSelector abstract class.

--- Assets/Resources/Scripts/LevelEditor.cs
+++ Assets/Resources/Scripts/LevelEditor.cs
@@ -5,16 +5,18 @@ using System.Collections.Generic;
 
 public class LevelEditor : MonoBehaviour {
 	
-	public static readonly int WIDTH = 100;
-	public static readonly int HEIGHT = 100;
+	public static int WIDTH = 100;
+	public static int HEIGHT = 100;
 
 	public static Grid floor;
 	public static int level = 1;
 	public static GameObject plane;
 
+	public static string objectToBeCreated;
+
 	void Start() {
 		plane = GameObject.CreatePrimitive(PrimitiveType.Plane);
-		plane.transform.localScale = new Vector3(1.25f, 1.0f, .4f);
+		plane.transform.localScale = new Vector3(, 1.0f, .4f);
 		plane.transform.Rotate(-90f, 0, 0);
 		//plane.renderer.material.mainTexture = Resources.Load("Textures");
 		
