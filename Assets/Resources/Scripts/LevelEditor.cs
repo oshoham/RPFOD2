dc36a9813f33d8f8b0cdc35565609685f525304d
vorDd <k.e.aungst@gmail.com>
2012-11-07 17:59:07 -0500

    LevelEditor

--- Assets/Resources/Scripts/LevelEditor.cs
+++ Assets/Resources/Scripts/LevelEditor.cs
@@ -5,8 +5,8 @@ using System.Collections.Generic;
 
 public class LevelEditor : MonoBehaviour {
 	
-	public static readonly int WIDTH = 100;
-	public static readonly int HEIGHT = 100;
+	public static int WIDTH = 100;
+	public static int HEIGHT = 100;
 
 	public static Grid floor;
 	public static int level = 1;
@@ -16,8 +16,21 @@ public class LevelEditor : MonoBehaviour {
 		plane = GameObject.CreatePrimitive(PrimitiveType.Plane);
 		plane.transform.localScale = new Vector3(1.25f, 1.0f, .4f);
 		plane.transform.Rotate(-90f, 0, 0);
-		//plane.renderer.material.mainTexture = Resources.Load("Textures");
+		plane.renderer.material.mainTexture = Resources.Load("Textures/Tile2") as Texture;
+		Camera.main.orthographic = true;
+		Camera.main.backgroundColor = Color.white;
+		string filename = EditorUtility.OpenFilePanel("Level file", "", "txt");
+		LevelLoader.LoadLevel(filename);
+		LevelWriter.WriteLevel(filename);
+		Debug.Log("Level Written.");
+		LevelLoader.LoadLevel(filename);
+		GameObject light = new GameObject("Light");
+		Light l = light.AddComponent<Light>();
+		light.transform.position = Camera.main.transform.position;
+		l.type = LightType.Directional;
+		l.intensity = 0.4f;
 		
 	}
 
+	void Update() {}
 }
