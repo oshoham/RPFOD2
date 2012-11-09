f3be691eb7749ddf979b20295a17777400b2976e
negtrooper1 <negtrooper0@yahoo.com>
2012-11-09 00:35:04 -0500

    Made Start Screen along with scripts to go with it.

---
+++ Assets/Resources/Scripts/StartScreenEditorButton.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using System.Collections;
+
+public class StartScreenEditorButton : MonoBehaviour {
+       GameObject editor;
+       public static int defaultFontSize;
+       public static int resizeTo = 50;
+
+       void Start() {
+       	    editor = GameObject.Find("Level Editor");
+       	    defaultFontSize = editor.guiText.fontSize;
+       }
+
+       void OnMouseEnter() {
+       	    editor.guiText.fontSize = resizeTo;
+       }
+
+       void OnMouseExit() {
+       	    editor.guiText.fontSize = defaultFontSize;
+       }
+
+       void OnMouseDown() {
+       	    Application.LoadLevel("Editor");
+       }
+}      
