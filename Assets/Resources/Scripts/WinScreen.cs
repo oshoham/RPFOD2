9a556efa3c67173a2479d74ca69765394d8fe91d
oshoham <oshoham@oberlin.edu>
2012-11-15 22:51:19 -0500

    Made it so that WinScreen actually takes you back to the Start menu.

--- Assets/Resources/Scripts/WinScreen.cs
+++ Assets/Resources/Scripts/WinScreen.cs
@@ -1,24 +1,17 @@
 using UnityEngine;
 
 public class WinScreen : MonoBehaviour {
-	GameObject game;
-	public static int defaultFontSize;
-	public static int resizeTo = 50;
 	
 	void Start() {
-		game = GameObject.Find("Winner Text");
-		defaultFontSize = game.guiText.fontSize;
-	}
-	
-	void OnMouseEnter() {
-		game.guiText.fontSize = resizeTo;
-	}
-	
-	void OnMouseExit() {
-		game.guiText.fontSize = defaultFontSize;
-	}
-	
-	void OnMouseDown() {
-		Application.LoadLevel("StartScreen");
+		GameObject winButton = new GameObject("Win Button");
+		GUIText win = (GUIText)winButton.AddComponent(typeof(GUIText));
+		win.text = "A winner is you!";
+		win.anchor = TextAnchor.UpperLeft;
+		win.alignment = TextAlignment.Left;
+		win.lineSpacing = 1.0F;
+		win.font = (Font)Resources.Load("Fonts/ALIEN5");
+		win.fontSize = 40;
+		winButton.transform.position = new Vector3(0.2F, 0.75F, 0.0F);
+		winButton.AddComponent<BackButton>();
 	}
 }
