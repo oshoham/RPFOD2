93c7ef3115dd1d0ae4fa3ce95f0391297f997d9c
negtrooper1 <negtrooper0@yahoo.com>
2012-11-19 13:33:50 -0500

    added a few more audio effects and attempted to do some Thread.Sleep, but it didn't work so I took it out

--- Assets/Resources/Scripts/FreePlay.cs
+++ Assets/Resources/Scripts/FreePlay.cs
@@ -1,30 +1,35 @@
 using UnityEngine;
 using System.Collections;
+using System.Threading;
 
 public class FreePlay : MonoBehaviour {
-       GameObject game;
-       public static int defaultFontSize;
-       public static int resizeTo = 50;
-       public AudioClip tickOnSelect = Resources.Load("Audio/Effects/robotshot") as AudioClip;	
-       public AudioSource effects = new AudioSource();
+	GameObject game;
+	public static int defaultFontSize;
+	public static int resizeTo = 50;
+	public AudioClip tickOnClick = Resources.Load("Audio/Effects/robotshot") as AudioClip;	
+	public AudioClip tickOnHover = Resources.Load("Audio/Effects/tick") as AudioClip;
+	public AudioSource effects = new AudioSource();
 
-       void Start() {
-       	    game = GameObject.Find("Free Play");
-       	    defaultFontSize = game.guiText.fontSize;
-	    effects = (AudioSource)this.gameObject.AddComponent(typeof(AudioSource));
-       }
+	void Start() {
+		game = GameObject.Find("Free Play");
+		defaultFontSize = game.guiText.fontSize;
+		effects = (AudioSource)this.gameObject.AddComponent(typeof(AudioSource));
+	}
+	
+	void OnMouseEnter() {
+		game.guiText.fontSize = resizeTo;
+		effects.clip = tickOnClick;
+		effects.Play();
+	}
+	
+	void OnMouseExit() {
+		game.guiText.fontSize = defaultFontSize;
+	}
+	
+	void OnMouseDown() {
+		effects.clip = tickOnClick;
+		effects.Play();
+		Application.LoadLevel("FreePlaySelector");
+	}	
 
-       void OnMouseEnter() {
-       	    game.guiText.fontSize = resizeTo;
-	    effects.clip = tickOnSelect;
-	    effects.Play();
-       }
-
-       void OnMouseExit() {
-       	    game.guiText.fontSize = defaultFontSize;
-       }
-
-       void OnMouseDown() {
-       	    Application.LoadLevel("FreePlaySelector");
-       }
 }
