717a50db44a4099127f58483e82f02babb673df7
peter-fogg <peter.fogg@oberlin.edu>
2012-11-26 15:02:19 -0500

    Fixed winchecking not working.

--- Assets/Resources/Scripts/WinChecker.cs
+++ Assets/Resources/Scripts/WinChecker.cs
@@ -3,27 +3,28 @@ using UnityEngine;
 public class WinChecker : MonoBehaviour {
 
 	// Is the number of robots a win condition?
-	public static bool robotsWin = false;
+	public static bool robotsWin;
 	// The number of robots in the game.
-	public static int numRobots = 0;
+	public static int numRobots;
 	// The number at which we win.
-	public static int robotLimit = -1;
+	public static int robotLimit;
 
 	// Is there a win square?
-	public static bool squareWins = false;
+	public static bool squareWins;
 	// The square at which we'll win;
 	public static Vector2 winCoords;
-
+	
 	void Update() {
+		print(numRobots);
 		if(robotsWin && squareWins &&
 		   numRobots <= robotLimit &&
 		   GameManager.player.gridCoords == winCoords) {
 			GameManager.Win();
 		}
-		if(robotsWin && numRobots <= robotLimit) {
+		else if(robotsWin && numRobots <= robotLimit) {
 			GameManager.Win();
 		}
-		if(squareWins && GameManager.player.gridCoords == winCoords) {
+		else if(squareWins && GameManager.player.gridCoords == winCoords) {
 			GameManager.Win();
 		}
 	}
